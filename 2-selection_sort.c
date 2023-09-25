49e1f624d840cbc05cb1ea9d0923478f655f7cbc
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-25 14:56:25 -0700

    implementing selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,27 +1,35 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sorts an array using selection sort
+ * @array: array to be sorted
+ * @size: size of array
+ */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t j, min, i;
+	size_t j, min_index, i;
 	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
+		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if(array[j] < array[min])
-				min = j;
+			if (array[j] < array[min_index])
+				min_index = j;
 		}
 
-		if (min != i)
+		if (min_index != i)
 
 		{
 			tmp = array[i];
-			array[i] = array[min];
-			array[min] = tmp;
+			array[i] = array[min_index];
+			array[min_index] = tmp;
 			print_array(array, size);
 		}
 
 	}
 }
+
+
