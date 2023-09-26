0770317b3782f75dcc9bef8c5ef524ecb3459c8a
Chidimma Ofodum <chidimmamofodum@gmail.com>
2023-09-25 18:09:59 -0700

    insertion and helper funnction in a single file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 #include <limits.h>
+void insert_min_node_head(listint_t **list);
 /**
  * insertion_sort_list - sorts a doubly linked list using insertion sort
  * @list: address of the head of the list
@@ -52,4 +53,26 @@ void insertion_sort_list(listint_t **list)
 	*list = (*list)->next;
 	free(tmp);
 	(*list)->prev = NULL;
+}
+
+/**
+ * insert_min_node_head - inserts dummy node at head of list
+ * @list: the list
+ *
+ * Return: alway void
+ */
+void insert_min_node_head(listint_t **list)
+{
+        listint_t *node;
+        int *temp;
+
+        node = malloc(sizeof(listint_t));
+        if (!node)
+                return;
+        temp = (int *)&node->n;
+        *temp = INT_MIN;
+        (*list)->prev = node;
+        node->next = *list;
+        node->prev = NULL;
+        *list = node;
 }
